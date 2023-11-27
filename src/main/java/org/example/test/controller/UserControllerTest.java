e28679265074482e459eb31abc31b833ab39ac6d
kgypeter <kgyulapeter10@gmail.com>
2023-11-27 13:25:03 +0200

    fixed tests for the backend modifications

--- src/main/java/org/example/test/controller/UserControllerTest.java
+++ src/main/java/org/example/test/controller/UserControllerTest.java
@@ -1,6 +1,7 @@
 package org.example.test.controller;
 
 import org.example.controller.UserController;
+import org.example.model.ERole;
 import org.example.model.User;
 import org.example.service.AccountHistoryService;
 import org.example.service.UserService;
@@ -58,18 +59,27 @@ class UserControllerTest {
     }
 
     @Test
-    void updateUser_ValidUser_ReturnsUpdatedUser() {
+    void updateUser_ValidUser_ReturnsOK() {
         // Mocking
-        User user = new User(); // Create a valid user
-        when(userService.updateUser(user)).thenReturn(user);
+        User user = new User();
+        user.setLastname("Doe");
+        user.setFirstname("John");
+        user.setEmail("john@example.com");
+        user.setPassword("password");
+        user.setRole(ERole.STD); // Set a role for the user
+
+        when(userService.updateUser(any(User.class))).thenReturn(user);
 
         // Test
-        //ResponseEntity<User> response = userController.updateUser(user.getLastname(), user.getFirstname(), user.getEmail(), user.getPassword(), user.getRole().toString());
+        ResponseEntity<User> response = userController.updateUser(
+                user.getLastname(), user.getFirstname(), user.getEmail(), user.getPassword(), user.getRole().toString()
+        );
 
         // Assertion
-        //assertEquals(HttpStatus.OK, response.getStatusCode());
-        //assertEquals(user, response.getBody());
-        //verify(userService, times(1)).updateUser(user);
-        //verify(accountHistoryService, times(1)).makeNewEntry(any(), any());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(user, response.getBody());
+        verify(userService, times(1)).updateUser(any(User.class));
     }
+
+
 }
